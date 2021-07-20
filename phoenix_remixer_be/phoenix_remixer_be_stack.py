7bb805c4458ce5e79a840c9b599a99eb349ed9f3
nroldanf <nicolas.roldan@mail.escuelaing.edu.co>
2021-07-19 23:37:44 -0500

    sqs deployed without kms cmk

--- phoenix_remixer_be/phoenix_remixer_be_stack.py
+++ phoenix_remixer_be/phoenix_remixer_be_stack.py
@@ -1,4 +1,7 @@
+from os import path
+
 from aws_cdk import core
+from aws_cdk.aws_s3_assets import Asset
 from ec2 import AwsEc2
 from s3 import AwsS3
 from sqs import AwsSQS
@@ -22,4 +25,16 @@ class PhoenixRemixerBeStack(core.Stack):
         AwsSQS(self).sqs(parameters.get("name"))
 
         # EC2 instance within a VPC
-        AwsEc2(self).ec2(parameters.get("name"))
+        ec2 = AwsEc2(self).ec2(parameters.get("name"))
+
+        # Script in S3 as Asset
+        dirname = path.dirname(__file__)
+        asset = Asset(self, "Asset", path=path.join(dirname, "ec2_asset.py"))
+        _ = ec2.user_data.add_s3_download_command(
+            bucket=asset.bucket, bucket_key=asset.s3_object_key
+        )
+        # # Userdata executes script from S3
+        # ec2.user_data.add_execute_file_command(
+        #     file_path=local_path
+        #     )
+        # asset.grant_read(ec2.role)
--- phoenix_remixer_be/sqs.py
+++ phoenix_remixer_be/sqs.py
@@ -1,3 +1,4 @@
+from aws_cdk import aws_iam as iam
 from aws_cdk import aws_kms as kms
 from aws_cdk import aws_sqs as sqs
 from aws_cdk import core
@@ -14,24 +15,97 @@ class AwsSQS:
         scope: core.Construct,
         fifo: bool = False,
         retention_period: int = core.Duration.days(1),
+        removal_policy: core.RemovalPolicy = core.RemovalPolicy.DESTROY,
     ) -> None:
         self.scope = scope
         self.retention_period = retention_period
-        self.fifo = fifo
+        self.fifo = True if fifo else None
+        self.removal_policy = removal_policy
+
+    def key_policy(self) -> iam.PolicyDocument:
+        """
+        https://docs.aws.amazon.com/cdk/api/latest/python/aws_cdk.aws_kms/Key.html
+        https://aws.amazon.com/es/premiumsupport/knowledge-center/update-key-policy-future/
+        """
+        policy_document = iam.PolicyDocument(
+            statements=[
+                iam.PolicyStatement(
+                    sid="Allow administration of the key",
+                    effect=iam.Effect.ALLOW,
+                    # principals=[
+                    #     iam.ArnPrincipal(f"aws:iam::{core.Aws.ACCOUNT_ID}:nrf-cdk"),
+                    # ],
+                    principals=[iam.AccountPrincipal(core.Aws.ACCOUNT_ID)],
+                    actions=[
+                        "kms:Create*",
+                        "kms:Describe*",
+                        "kms:Enable*",
+                        "kms:List*",
+                        "kms:Put*",
+                        "kms:Update*",
+                        "kms:Revoke*",
+                        "kms:Disable*",
+                        "kms:Get*",
+                        "kms:Delete*",
+                        "kms:ScheduleKeyDeletion",
+                        "kms:CancelKeyDeletion",
+                    ],
+                    resources=["*"],
+                ),
+                iam.PolicyStatement(
+                    sid="Allow use of the key",
+                    effect=iam.Effect.ALLOW,
+                    # principals=[
+                    #     iam.ArnPrincipal(f"aws:iam::{core.Aws.ACCOUNT_ID}:nrf-cdk"),
+                    # ],
+                    principals=[iam.AccountPrincipal(core.Aws.ACCOUNT_ID)],
+                    actions=[
+                        "kms:Encrypt",
+                        "kms:Decrypt",
+                        "kms:ReEncrypt*",
+                        "kms:GenerateDataKey*",
+                        "kms:DescribeKey",
+                    ],
+                    resources=["*"],
+                ),
+                iam.PolicyStatement(
+                    sid="Allow attachment of persistent resources",
+                    effect=iam.Effect.ALLOW,
+                    principals=[iam.AccountPrincipal(core.Aws.ACCOUNT_ID)],
+                    actions=[
+                        "kms:CreateGrant",
+                        "kms:ListGrants",
+                        "kms:RevokeGrant",
+                    ],
+                    resources=["*"],
+                    conditions={"Bool": {"kms:GrantIsForAWSResource": "true"}},
+                ),
+            ]
+        )
+        return policy_document
 
     def kms_key(self, name) -> kms.Key:
-        key = kms.Key(self.scope, f"{name}-key")
+        policy = self.key_policy()
+        key = kms.Key(
+            self.scope,
+            f"{name}-key",
+            description="Remixer SQS CMK for encryption",
+            policy=policy,
+            removal_policy=self.removal_policy,
+        )
         return key
 
     def sqs(self, name: str) -> sqs.Queue:
         name = f"{name}-queue.fifo" if self.fifo else f"{name}-queue"
-        key = self.kms_key(name)
+        # encryption_master_key = self.kms_key(name)
         queue = sqs.Queue(
             self.scope,
             name,
-            encryption=sqs.QueueEncryption.KMS,
-            encryption_master_key=key,
+            queue_name=name,
+            encryption=sqs.QueueEncryption.KMS_MANAGED,
+            # encryption_master_key=encryption_master_key,
             fifo=self.fifo,
             retention_period=self.retention_period,
+            removal_policy=self.removal_policy,
         )
         return queue
